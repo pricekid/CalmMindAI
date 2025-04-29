8ded9844e5268ae461bef55f227a31edce59d157
naturalarts <38405744-naturalarts@users.noreply.replit.com>
2025-04-29 23:39:45 +0000

    Restored to '3e5302efa1d81865ca35ef1d961612d9bc558012'
    Replit-Restored-To: 3e5302efa1d81865ca35ef1d961612d9bc558012

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--port', type=int, default=5000)
+    parser.add_argument('--port', type=int, default=8000)
     args = parser.parse_args()
 
     # Start the web application
